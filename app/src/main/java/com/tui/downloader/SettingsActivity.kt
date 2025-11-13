cb75655de4c51997851d72af89e610e56a1bf0ba
Rikunss <87461601+Rikunss@users.noreply.github.com>
2025-11-13 22:17:19 +0700

    Update SettingsActivity.kt

--- app/src/main/java/com/tui/downloader/SettingsActivity.kt
+++ app/src/main/java/com/tui/downloader/SettingsActivity.kt
@@ -2,9 +2,22 @@ package com.tui.downloader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tui.downloader.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Load preference fragment
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.settingsContainer.id, SettingsFragment())
+            .commit()
     }
 }
