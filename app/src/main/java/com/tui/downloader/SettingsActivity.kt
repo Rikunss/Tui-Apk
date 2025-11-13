701a65bb590f1565ac855bc9180414daa87b1e8b
Rikunss <87461601+Rikunss@users.noreply.github.com>
2025-11-13 22:05:04 +0700

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/tui/downloader/SettingsActivity.kt
@@ -0,0 +1,10 @@
+package com.tui.downloader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
